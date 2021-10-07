6cedf7b8e0ad4a35a372aa4f0b1a227185ed3607
Joan Guitart <joan.guiroc@gmail.com>
2021-10-07 23:36:15 +0200

    Home animations

---
+++ js/animations.js
@@ -0,0 +1,72 @@
+gsap.from('#bg-item-1', {
+    delay: .2,
+
+    duration: 1,
+    x:500,
+    opacity: 0,
+})
+
+gsap.from('#bg-item-2', {
+    delay: .5,
+    duration: 1,
+    x:500,
+    opacity: 0,
+})
+
+gsap.from('#bg-item-3', {
+    delay: .4,
+    duration: 1,
+    x:500,
+    opacity: 0,
+})
+
+
+
+gsap.from('#main-title', {
+    delay: 1.5,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('#secon-title', {
+    delay: 1.7,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('#valor', {
+    delay: 1.8,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('#btn-home', {
+    delay: 1.8,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('#home-img', {
+    delay: 1.7,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('#home-img-bg', {
+    delay: 1.9,
+    duration: 1,
+    x:-100,
+    opacity: 0,
+})
+
+gsap.from('.sidebar', {
+    delay: 2.5,
+    duration: 1,
+    opacity: 0,
+})
+
